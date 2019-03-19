9ad95ea6710916575b8fed33360272ecc55ad395
LaurentBouchard <lbouchard11@gmail.com>
2019-03-19 13:59:53 -0400

    Modified pretrained_example.py to generate waifus without having to edit the code manually Please download Gwern's model before running (https://mega.nz/#!aPRFDKaC!FDpQi_FEPK443JoRBEOEDOmlLmJSblKFlqZ1A1XPt2Y)

--- pretrained_example.py
+++ pretrained_example.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
-#
+# Copyright (c) 2019, Laurent Bouchard. The modifications made in this file were made by myself and are not endorsed by NVlabs0
+# The original work can be found here: https://github.com/NVlabs/stylegan
+# 
 # This work is licensed under the Creative Commons Attribution-NonCommercial
 # 4.0 International License. To view a copy of this license, visit
 # http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
@@ -14,34 +16,41 @@ import PIL.Image
 import dnnlib
 import dnnlib.tflib as tflib
 import config
+import sys
 
 def main():
+    if (len(sys.argv) != 4):
+        print("Usage: python", str(sys.argv[0]), "<name of the round> <psi> <number of images>\n Name of the round can be any string and psi should be a number between 0.0 and 1.0 and the number of image can be any integer")
+        exit(1)
+    
+    # Pick parameters.
+    round = str(sys.argv[1])
+    psi = float(sys.argv[2])
+    nb_images = int(sys.argv[3])
+
     # Initialize TensorFlow.
     tflib.init_tf()
-
+    
     # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
-        _G, _D, Gs = pickle.load(f)
-        # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
-        # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
-        # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
+    _G, _D, Gs = pickle.load(open("./2019-02-26-stylegan-faces-network-02048-016041.pkl", "rb"))
 
-    # Print network details.
-    Gs.print_layers()
-
-    # Pick latent vector.
-    rnd = np.random.RandomState(5)
-    latents = rnd.randn(1, Gs.input_shape[1])
-
-    # Generate image.
-    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
-
-    # Save image.
     os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
-    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+    os.makedirs(os.path.join(config.result_dir, round))
+    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    rnd = np.random.RandomState(None)
+    
+    print("Generating images...\n")
+    for i in range(0,nb_images):
+        latents = rnd.randn(1, Gs.input_shape[1])
+
+        # Generate image.
+        images = Gs.run(latents, None, truncation_psi=psi, randomize_noise=True, output_transform=fmt)
+    
+        # Save image.
+        png_filename = os.path.join(config.result_dir, round, str(i)+'.png')
+        PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+    
+    print("\nImages generation done. They can be found in", "./results/"+round+"/")
 
 if __name__ == "__main__":
     main()
